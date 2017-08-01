f4545959598e0ae969005a6966e0c347ef14ff03
ShuguangWang <wangshuguang@inspur.com>
2017-08-01 17:47:20 +0800

    update osd.cc

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -1,3 +1,4 @@
+
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 /*
@@ -57,6 +58,8 @@
 #include "mon/MonClient.h"
 
 #include "messages/MLog.h"
+
+
 
 #include "messages/MGenericMessage.h"
 #include "messages/MPing.h"
